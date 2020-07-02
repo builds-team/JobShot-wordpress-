d225ad1a7cf886187fe268ba7a28c7f37dd4f89a
Genya Nobuhara <ball_19_0407@yahoo.co.jp>
2020-07-02 22:18:25 +0900

    analytics

--- analytics.php
+++ analytics.php
@@ -12,6 +12,32 @@ function google_analytics_add_wp_head (){
 }
 add_action ('wp_head','google_analytics_add_wp_head',1);
 
+function google_analytics_add_wp_head (){
+  echo "<!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src='https://www.googletagmanager.com/gtag/js?id=UA-135934453-1'></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-135934453-1', {'optimize_id':'GTM-5J6DKXZ'});
+  </script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WRXPBDJ');</script>
+<!-- End Google Tag Manager -->
+<!-- User Heat Tag -->
+<script type='text/javascript'>
+(function(add, cla){window['UserHeatTag']=cla;window[cla]=window[cla]||function(){(window[cla].q=window[cla].q||[]).push(arguments)},window[cla].l=1*new Date();var ul=document.createElement('script');var tag = document.getElementsByTagName('script')[0];ul.async=1;ul.src=add;tag.parentNode.insertBefore(ul,tag);})('//uh.nakanohito.jp/uhj2/uh.js', '_uhtracker');_uhtracker({id:'uhAInVkJCv'});
+</script>
+<!-- End User Heat Tag -->";
+
+}
+add_action ('wp_head','google_analytics_add_wp_head',1);
+
+
 function instagram_analytics_add_wp_head (){
     echo "
     <!-- Facebook Pixel Code -->
